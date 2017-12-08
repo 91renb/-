f77d08204c1f5c74a2160a65195517f0b65833da
任波 <borenfocus@gmail.com>
2017-12-08 13:30:11 +0800

    更新代码

---
+++ 测试复合项目/TestB/TestB/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestB
+//
+//  Created by 任波 on 2017/12/7.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
